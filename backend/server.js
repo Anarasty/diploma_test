0648cb1f56d67c28a11e2159726927a0f1259926
Anarasty <murloc.ninja16@gmail.com>
2023-02-02 16:31:13 +0200

    conecting products to mongo db

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -15,28 +17,14 @@ mongoose
   });
 
 const app = express();
+app.use("/api/seed", seedRouter);
+// test
+app.use("/api/products", productRouter);
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
 
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product missing!" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
