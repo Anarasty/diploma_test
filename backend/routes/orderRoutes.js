08cba5a160a9762c2046420528769977fe85decd
Anarasty <murloc.ninja16@gmail.com>
2023-02-20 17:34:18 +0200

    order screen added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -15,12 +15,25 @@ orderRouter.post(
       itemsPrice: req.body.itemsPrice,
       shippingPrice: req.body.shippingPrice,
       taxPrice: req.body.taxPrice,
-      totalPrice: req.body.totalPrice,  
+      totalPrice: req.body.totalPrice,
       user: req.user._id,
     });
 
     const order = await newOrder.save();
     res.status(201).send({ message: "New Order Created", order });
+  })
+);
+
+orderRouter.get(
+  "/:id",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
   })
 );
 export default orderRouter;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -84,6 +85,7 @@ function App() {
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
               <Route
                 path="/shipping"
                 element={<ShippingAddressScreen />}
